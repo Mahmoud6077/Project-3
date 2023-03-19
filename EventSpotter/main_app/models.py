d08e614f03b7cd2707436dc77d5c916510351d47
Mahmood Ibrahim <mebrahim6077@gmail.com>
2023-03-19 10:26:44 +0300

    Updating the DB and rest files

--- EventSpotter/main_app/models.py
+++ EventSpotter/main_app/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.urls import reverse
+#for Auth
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Events(models.Model):
     name = models.CharField(max_length = 150)
     location = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 250)
     date = models.DateField()
     time = models.TimeField()
+    image = models.ImageField(upload_to ='main_app/static/images/', default="")
     def __str__(self):
         return self.name
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # def get_absolute_url(self):
     #     return reverse('toys_detail', kwargs={'pk': self.id})
 
--- EventSpotter/main_app/views.py
+++ EventSpotter/main_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Add the following import
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Define the home view
 def home(request):
@@ -12,3 +15,26 @@ def home(request):
 def base(request):
     # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
     return render(request, 'base.html')
+
+def signup(request):
+    error_message =''
+    if request.method == 'POST':
+        #Make a 'User' form object with the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Save user to DB
+            user = form.save()
+            #login the user automaticlly once they signed up 
+            login(request, user)
+            return redirect('index')
+        
+        else:
+            error_message = 'Invalid: Please Try Again'
+    #if there is a bad post or a get request
+    form = UserCreationForm()
+    context = {
+        'form': form,
+        'error_message': error_message
+    }
+    return render(request, 'registration/signup.html', context)
+
