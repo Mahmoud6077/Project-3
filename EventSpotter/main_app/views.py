027752ef71191a57d9f7831917d15d7fbb13d70b
msigp60 <ahmedalslaibi@gmail.com>
2023-03-19 10:52:23 +0300

    testing

--- EventSpotter/main_app/views.py
+++ EventSpotter/main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
-
+##
 # Define the home view
 def home(request):
     # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -12,3 +12,4 @@ def home(request):
 def base(request):
     # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
     return render(request, 'base.html')
+
